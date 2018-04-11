2a91fe6b1c54bb8a01fbe6b965e9a0b4f1c619b1
Lukas Gross <lu851gro@htwg-konstanz.de>
2018-04-11 11:13:05 +0200

    improved prize format

--- src/main/java/de/htwg/da/esper/sample/listener/KickOutListener.java
+++ src/main/java/de/htwg/da/esper/sample/listener/KickOutListener.java
@@ -6,9 +6,10 @@ import com.espertech.esper.client.UpdateListener;
 public class KickOutListener implements UpdateListener{
 
     public void update(EventBean[] newData, EventBean[] oldDate) {
-        final Object prize_sum = newData[0].get("prize_sum");
+        final Integer prize_sum = (Integer) newData[0].get("prize_sum");
         final Object playerName = newData[0].get("playerName");
 
-        System.out.println("------> Player '" + playerName + "' has won too much '"+ prize_sum +"' - kick him/her out!");
+        String formattedSum = String.format("%,d", prize_sum);
+        System.out.println("------> Player '" + playerName + "' has won too much '"+ formattedSum +"' - kick him/her out!");
     }
 }
